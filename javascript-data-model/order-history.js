3af4f86641bac15ed9aab1e6fa6d2e0e1113a5ba
Frank Bishop <ffbishop3@gmail.com>
2021-01-26 12:34:59 -0800

    JavaScript Data Model Fourth Attempt

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,12 +2,12 @@ var orderHistory = [
   {
     orderNumber: '114-3941689-8772232',
     orderPlaced: 'August 4, 2020',
-    orderTotal: '$34.00',
+    orderTotal: 34.00,
     shipTo: 'JS Masher',
     items: [{
       name: 'Javascript for Impatient Programmers',
       by: 'Dr. Axel Rauschmayer',
-      price: '$31.55',
+      price: 31.55,
       deliveredOn: 'August 8, 2020',
       returnWindowEnd: 'September 7, 2020'
     }
@@ -16,12 +16,12 @@ var orderHistory = [
   {
     orderNumber: '113-9984268-128057',
     orderPlaced: 'July 19, 2020',
-    orderTotal: '$44.53',
+    orderTotal: 44.53,
     shipTo: 'JS Masher',
     items: [{
       name: 'The Timeless Way of Building',
       by: 'Christoper Alexander',
-      price: '$41.43',
+      price: 41.43,
       deliveredOn: 'July 20, 2020',
       returnWindowEnd: 'August 19, 2020'
     }
@@ -30,11 +30,11 @@ var orderHistory = [
   {
     orderNumber: '114-2875557-9059409',
     orderPlaced: 'July 4, 2020',
-    orderTotal: '$17.22',
+    orderTotal: 17.22,
     shipTo: 'JS Masher',
     items: [{
       name: 'Gamecube Controller Adapter.Super Smash Bros.Switch Gamecube Adapter for Wii U, PC.Support Turbo and Vibration Features.No Driver and No Lag Gamecube adapter',
-      price: '15.98',
+      price: 15.98,
       deliveredOn: 'July 7, 2020',
       returnWindowEnd: 'August 5, 2020'
     }
@@ -43,18 +43,18 @@ var orderHistory = [
   {
     orderNumber: '113-2883177 - 2648248',
     orderPlaced: 'July 3, 2020',
-    orderTotal: '$138.93',
+    orderTotal: 138.93,
     shipTo: 'JS Masher',
     items: [{
       name: 'GameCube Controller - Super Smash Bros.Edition(Nintendo Switch)',
-      price: '94.95',
+      price: 94.95,
       deliveredOn: 'July 5, 2020',
       returnWindowEnd: 'August 4, 2020'
     },
     {
       name: 'The Art of SQL',
       by: 'Stephane Faroult',
-      price: '$33.99',
+      price: 33.99,
       deliveredOn: 'July 5, 2020',
       returnWindowEnd: 'August 4, 2020'
     }
