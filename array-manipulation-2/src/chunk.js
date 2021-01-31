4199864b0833908511dd3d5c6970ae3783a76ca4
Frank Bishop <ffbishop3@gmail.com>
2021-01-31 09:57:14 -0800

    2nd In Progress Array Manipulation 2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,7 +1,14 @@
 /* exported chunk */
 function chunk(array, size) {
   var finalArray = [];
-  var miniArray = array.slice(size);
-  finalArray = miniArray;
+  // var miniArray = array.slice(size);
+  var miniArray = [];
+  var i;
+  for (i = 0; i <= array.length; i += size) {
+    miniArray = array[i];
+    finalArray.push(miniArray);
+  }
   return finalArray;
 }
+
+// a mix of slice and push
