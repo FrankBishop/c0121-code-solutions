5a2d56e93ca6ce5b21c8490de07f1855bfbf92d0
Frank Bishop <ffbishop3@gmail.com>
2021-01-30 17:29:10 -0800

    In Progress Array Manipulation 2

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,44 @@
 /* exported includes */
+// function includes(array, value) {
+//   var i;
+//   var theValue = array[i];
+//   for (i = 0; i <= array.length; i++) {
+//     if (theValue === value) {
+//       return true;
+//     } else {
+//       find = false
+//     }
+//   }
+//   return find
+// }
+// function includes(array, value) {
+//   var i;
+//   for (i = 0; i <= array.length; i++) {
+//     if (array.find(value) === i) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+// }
+// function includes(array, value) {
+//   var found = if(array.find(value));
+// var i;
+// for (i = 0; i <= array.length; i++) {
+//   if (array.find(value) === i) {
+//     return true
+//   } else {
+//     return false
+//   }
+//   return found
+// }
+function includes(array, value) {
+  var found = array.indexOf(value);
+  var result = false;
+  if (found >= 0) {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+}
