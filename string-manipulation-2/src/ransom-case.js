f40acc6403793640dbe8bf6d318e3c55bcf7c97f
Frank Bishop <ffbishop3@gmail.com>
2021-02-02 11:38:07 -0800

    sm2

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,4 +1,22 @@
 /* exported ransomCase */
+// function ransomCase(string) {
+//   var newWord;
+//   var newWord2;
+//   var newWord3;
+//   var i;
+
+//   for (i = 0; i <= string.length; i++) {
+//     newWord = string.slice(0, 1);
+//     // newWord2 = string[1].toUpperCase();
+//     newWord2 = string.slice(1, 2);
+//     newWord2 = newWord2.toUpperCase();
+//     newWord3 = newWord + newWord2;
+//   }
+//   return newWord3;
+// }
+
+// if index % 2
+
 function ransomCase(string) {
   var newWord;
   var newWord2;
@@ -14,5 +32,3 @@ function ransomCase(string) {
   }
   return newWord3;
 }
-
-// if index % 2
