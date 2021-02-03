66f66feae75cabd9e14fa9200befd26122f5a3d4
Frank Bishop <ffbishop3@gmail.com>
2021-02-02 22:54:54 -0800

    finished JS modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,23 +1,3 @@
-// var openModal = document.querySelector('.open-modal-button');
-// var modal = document.querySelector('.modal');
-// var container = document.querySelector('.container');
-// var closeModal = document.querySelector('.modal-button');
-// var body = document.querySelector('.page');
-// openModal.addEventListener('click', openModalFunction);
-// closeModal.addEventListener('click', closeModalFunction);
-
-// function openModalFunction(event) {
-//   container.className = 'container-opened';
-//   body.className = 'modal-page';
-//   modal.className = 'modal-opened';
-//   openModal.className = 'open-modal-button-open';
-// }
-
-// function closeModalFunction(event) {
-//   modal.className = 'modal';
-//   container.className = 'container';
-//   body.className = 'modal-page';
-// }
 var openModal = document.querySelector('.open-modal-button');
 var modal = document.querySelector('.container');
 var page = document.querySelector('.page');
