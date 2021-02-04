1a0cd90d79db05698d89944ce89321d180380529
Frank Bishop <ffbishop3@gmail.com>
2021-02-04 10:55:26 -0800

    in progress typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,10 +3,21 @@
 
 // function keyTestStart(event) {
 //   for (i = 0; i < span.lenght; i++){
-//     span.addEventListener('keydown', keyTest)
+//     i.addEventListener('keydown', keyTest);
+//     if (keydown )
 //   }
 // }
 
 // function keyTest(event) {
 
 // }
+
+// what I need to do
+
+// create a for loop over all the spans
+// add event listener to them
+// make conditionals to tell if correct key was typed
+// if corrent key was typed, it is   green
+// if not red
+// also need line under each letter
+// keyboard event key to tell which key was pressed
