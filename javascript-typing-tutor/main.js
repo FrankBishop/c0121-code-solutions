e91f1db36868e5c68dc9dd82d096a66ec26c1e7e
Frank Bishop <ffbishop3@gmail.com>
2021-02-03 17:03:24 -0800

    in progress javascript-typing-tutor

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,12 @@
+// var span= document.querySelectorAll('span')
+// var i;
+
+// function keyTestStart(event) {
+//   for (i = 0; i < span.lenght; i++){
+//     span.addEventListener('keydown', keyTest)
+//   }
+// }
+
+// function keyTest(event) {
+
+// }
