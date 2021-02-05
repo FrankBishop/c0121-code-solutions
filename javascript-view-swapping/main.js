1cbacea214c7c0ff419b40aab70df8d60e28b500
Frank Bishop <ffbishop3@gmail.com>
2021-02-04 19:32:47 -0800

    finished javascript-view-swapping

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,27 @@
+var $tabcontainer = document.querySelector('.tab-container');
+var tabNodeList = document.querySelectorAll('.tab');
+var viewNodeList = document.querySelectorAll('.view');
+$tabcontainer.addEventListener('click', languagesTab);
+
+function languagesTab(event) {
+  var result = event.target.matches('.tab');
+  var i;
+  var j;
+  if (result) {
+    for (i = 0; i < tabNodeList.length; i++) {
+      if (tabNodeList[i] === event.target) {
+        tabNodeList[i].className = 'tab active';
+      } else {
+        tabNodeList[i].className = 'tab';
+      }
+    }
+  }
+  var data = event.target.getAttribute('data-view');
+  for (j = 0; j < viewNodeList.length; j++) {
+    if (viewNodeList[j].getAttribute('data-view') === data) {
+      viewNodeList[j].className = 'view';
+    } else {
+      viewNodeList[j].className = 'hidden';
+    }
+  }
+}
