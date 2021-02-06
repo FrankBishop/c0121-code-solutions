11eeb0481391c36c86fa9e518c8267357e4adc40
Frank Bishop <ffbishop3@gmail.com>
2021-02-06 13:01:20 -0800

    working typing tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,69 +1,22 @@
 var span = document.querySelectorAll('span');
-// document.addEventListener('keydown', keyTest);
-// var spanTest = document.querySelector('span');
-// var letters = document.querySelector('.typing-test').textContent;
+document.addEventListener('keydown', keyTest);
 
-// Put keydown in forEach for span;
+var spanIndexNumber = 0;
 
-// keydown function within for Loop;
+var spanIndex = span[spanIndexNumber].textContent;
 
-// var lettersArray = letters.split('');
-var i;
-// var currentLetter;
-// for (var i = 0; i < lettersArray.length; i++) {
-//   currentLetter = lettersArray[i];
-//   console.log(currentLetter);
-// }
+var spanChangeIndexNumber = 0;
+var spanChange = span[spanChangeIndexNumber];
 
-span.forEach(spanLoop);
-
-function spanLoop(node) {
-  for (i = 0; i <= node.length; i++) {
-  // console.log(node[i]);
-  // letters[i].addEventListener('keydown', keyTest);
+function keyTest(event) {
+  var key = event.key;
+  if (key === spanIndex) {
+    spanChange.className = 'greenkey';
+    spanIndexNumber++;
+    spanChangeIndexNumber++;
+    spanIndex = span[spanIndexNumber].textContent;
+    spanChange = span[spanChangeIndexNumber];
+  } else {
+    spanChange.className = 'redkey';
   }
-  // console.log(node)
-
 }
-
-// function keyTest(event) {
-//   var key = event.key;
-//   console.log(key);
-//   // var lettersArray[i] = lettersArray[0];
-//   if (key === lettersArray[i]) {
-//       spanTest.className = 'greenkey';
-//       lettersArray[i]++
-//     } else {
-
-//       spanTest.className = 'redkey';
-//     }
-//   }
-
-// span.forEach(spanLoop);
-// span.forEach(letters);
-
-// function spanLoop(node) {
-//   var i;
-//   for (i = 0; i <= span.length; i++) {
-//   console.log(letters[i]);
-//   letters[i].addEventListener('keydown', keyTest);
-//   }
-//   console.log(node)
-
-// }
-// if(key === "a") {
-//   console.log('a test worked');
-//   spanTest.className = 'greenkey';
-// } else if(key === "w") {
-//   console.log('this works too')
-// }
-
-// what I need to do
-
-// create a for loop over all the spans
-// add event listener to them
-// make conditionals to tell if correct key was typed
-// if corrent key was typed, it is   green
-// if not red
-// also need line under each letter
-// keyboard event key to tell which key was pressed
