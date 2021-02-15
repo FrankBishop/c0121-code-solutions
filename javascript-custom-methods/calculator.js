30f0d8a3988dcb31f11963f32af25942ff29988c
Frank Bishop <ffbishop3@gmail.com>
2021-02-15 14:44:39 -0800

    finished javascript-custom-methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,36 @@
+/* exported calculator */
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (numbers) {
+    var i = 0;
+    var total = 0;
+    for (i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    return total;
+  },
+  getAverage: function (numbers) {
+    var i = 0;
+    var total = 0;
+    for (i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    var result = total / numbers.length;
+    return result;
+  }
+
+};
