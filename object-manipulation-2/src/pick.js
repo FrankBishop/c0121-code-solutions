3d2919dad7afe04ac45672cfae05ac5ae384ea30
Frank Bishop <ffbishop3@gmail.com>
2021-02-14 16:12:48 -0800

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,31 +1,32 @@
 /* exported pick */
 // function pick(source, keys) {
-//   var newObject = Object.create(source);
-// newObject.array[0]= source.array[0];
-// var i;
-// for(key in keys){
-//   newObject.array = array[0];
-// }
-// for(i=0; i <= keys.length; i++) {
-//   newObject.keys = source.keys
-// }
-// var key;
-//   for (key in newObject){
-//     if(newObject !== keys) {
-//       delete newObject.key
-//     }
+//   var array = Object.entries(source);
+//   var newObject = {};
+//   var i;
+//   for(i=0; i <= array.length; i++) {
+//   var key = array[i][0];
+//   var value = array[i][1];
+//   newObject[key] = value;
+//   console.log(newObject);
 //   }
-//   return newObject;
+//   return newObject
 // }
+
+// need if statement for if array[i] === keys[i], and a i+=2 loop for it
+// add to new object or array
+// then add values
 function pick(source, keys) {
-  var newObject = Object.entries(source);
+  var array = Object.entries(source);
+  var newObject = {};
   var i;
-  for (i = 0; i < -newObject.length; i += 2) {
-    if (newObject[i] !== keys[i]) {
-      newObject.splice(i, 2);
+  for (i = 0; i <= array.length; i++) {
+    if (keys.includes(array[i])) {
+      // console.log('something works')
+
     }
 
   }
   return newObject;
-
 }
+
+// maybe combo of filter method and and includes method
