fa7a163e2724c4bd1fc4af01ca1828d2e47dc2bc
Frank Bishop <ffbishop3@gmail.com>
2021-02-17 15:34:53 -0800

    basic carousel loop working

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -4,7 +4,15 @@ var dot1 = document.querySelector('.dot1');
 var dot2 = document.querySelector('.dot2');
 var dot3 = document.querySelector('.dot3');
 var dot4 = document.querySelector('.dot4');
-var dot5 = document.querySelector('.dot5')
+var dot5 = document.querySelector('.dot5');
+var imageHolder = document.querySelector('img')
+var images = [
+  "images/001.png",
+  "images/004.png",
+  "images/007.png",
+  "images/025.png",
+  "images/039.png"
+]
 
 left.addEventListener('click', moveLeft);
 right.addEventListener('click', moveRight);
@@ -14,8 +22,29 @@ dot3.addEventListener('click', image3);
 dot4.addEventListener('click', image4);
 dot5.addEventListener('click', image5)
 
+// var imageCount = setInterval(imageLoop, 3000)
+// setInterval(imageLoop, 3000)
+
+function imageLoop() {
+  var i = 0;
+  setInterval(imageLoopAround, 3000)
+  function imageLoopAround() {
+    if (i < 5) {
+      var currentImage = imageHolder.getAttribute('src');
+      imageHolder.setAttribute('src', images[i]);
+      i++
+    } else {
+      i = 0;
+      currentImage = imageHolder.getAttribute('src');
+      imageHolder.setAttribute('src', images[i]);
+    }
+  }
+}
+
 function moveLeft(event) {
-  console.log('the left move')
+  console.log('the left move');
+
+  imageHolder.setAttribute('src', 'images/001.png');
 }
 
 function moveRight(event) {
@@ -24,20 +53,54 @@ function moveRight(event) {
 
 function image1(event) {
   console.log('image1')
+  imageHolder.setAttribute('src', 'images/001.png');
 }
 
 function image2(event) {
   console.log('image2')
+  imageHolder.setAttribute('src', 'images/004.png');
 }
 
 function image3(event) {
   console.log('image3')
+  imageHolder.setAttribute('src', 'images/007.png');
 }
 
 function image4(event) {
   console.log('image4')
+  imageHolder.setAttribute('src', 'images/025.png');
 }
 
 function image5(event) {
   console.log('image5')
+  imageHolder.setAttribute('src', 'images/039.png');
+}
+
+
+
+//set interval for when image changes
+//change image
+//assign image to each button
+function languagesTab(event) {
+  var result = event.target.matches('.tab');
+  var i;
+  var j;
+  if (result) {
+    for (i = 0; i < tabNodeList.length; i++) {
+      if (tabNodeList[i] === event.target) {
+        tabNodeList[i].className = 'tab active';
+        var data = event.target.getAttribute('data-view');
+        for (j = 0; j < viewNodeList.length; j++) {
+          if (viewNodeList[j].getAttribute('data-view') === data) {
+            viewNodeList[j].className = 'view';
+          } else {
+            viewNodeList[j].className = 'hidden';
+          }
+        }
+      } else {
+        tabNodeList[i].className = 'tab';
+      }
+    }
+  }
+
 }
