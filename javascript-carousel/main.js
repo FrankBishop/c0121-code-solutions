cd8f334fa7619159bda537cddf6060dc14d1b949
Frank Bishop <ffbishop3@gmail.com>
2021-02-17 16:52:11 -0800

    basic loop and choosing individual images are working

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -5,14 +5,16 @@ var dot2 = document.querySelector('.dot2');
 var dot3 = document.querySelector('.dot3');
 var dot4 = document.querySelector('.dot4');
 var dot5 = document.querySelector('.dot5');
-var imageHolder = document.querySelector('img')
+var imageHolder = document.querySelector('img');
 var images = [
   "images/001.png",
   "images/004.png",
   "images/007.png",
   "images/025.png",
   "images/039.png"
-]
+];
+var i = 0;
+var currentImage = imageHolder.getAttribute('src');
 
 left.addEventListener('click', moveLeft);
 right.addEventListener('click', moveRight);
@@ -22,85 +24,83 @@ dot3.addEventListener('click', image3);
 dot4.addEventListener('click', image4);
 dot5.addEventListener('click', image5)
 
-// var imageCount = setInterval(imageLoop, 3000)
-// setInterval(imageLoop, 3000)
+var pictureChange = setInterval(imageLoopAround, 3000)
 
-function imageLoop() {
-  var i = 0;
-  setInterval(imageLoopAround, 3000)
-  function imageLoopAround() {
-    if (i < 5) {
-      var currentImage = imageHolder.getAttribute('src');
-      imageHolder.setAttribute('src', images[i]);
-      i++
-    } else {
-      i = 0;
-      currentImage = imageHolder.getAttribute('src');
-      imageHolder.setAttribute('src', images[i]);
-    }
+function imageLoopAround() {
+  if (i < 5) {
+    currentImage = imageHolder.getAttribute('src');
+    imageHolder.setAttribute('src', images[i]);
+    i++
+  } else {
+    i = 0;
+    currentImage = imageHolder.getAttribute('src');
+    imageHolder.setAttribute('src', images[i]);
   }
 }
 
 function moveLeft(event) {
   console.log('the left move');
-
-  imageHolder.setAttribute('src', 'images/001.png');
+  if (i > 0) {
+    console.log('before', i);
+    i -= 1;
+    imageHolder.setAttribute('src', images[i]);
+    console.log('after set', i);
+    // imageLoopAround()
+  } else if (i <= 0) {
+    console.log('it is 0');
+    i = 4
+    imageHolder.setAttribute('src', images[i]);
+    // imageLoopAround()
+  }
 }
 
 function moveRight(event) {
-  console.log('the right move')
+  console.log('the right move');
+  if (i <= 3) {
+    console.log('before', i);
+    i+=1;
+    imageHolder.setAttribute('src', images[i]);
+    console.log('after set', i);
+    // imageLoopAround()
+  } else{
+    console.log('it is 4');
+    i = 0
+    imageHolder.setAttribute('src', images[i]);
+    // imageLoopAround()
+  }
 }
 
 function image1(event) {
-  console.log('image1')
   imageHolder.setAttribute('src', 'images/001.png');
+  clearInterval(pictureChange);
+  i = 0;
+  setInterval(imageLoopAround, 3000)
 }
 
 function image2(event) {
-  console.log('image2')
   imageHolder.setAttribute('src', 'images/004.png');
+  clearInterval(pictureChange);
+  i = 1;
+  setInterval(imageLoopAround, 3000)
 }
 
 function image3(event) {
-  console.log('image3')
   imageHolder.setAttribute('src', 'images/007.png');
+  clearInterval(pictureChange);
+  i = 2;
+  setInterval(imageLoopAround, 3000)
 }
 
 function image4(event) {
-  console.log('image4')
   imageHolder.setAttribute('src', 'images/025.png');
+  clearInterval(pictureChange);
+  i = 3;
+  setInterval(imageLoopAround, 3000)
 }
 
 function image5(event) {
-  console.log('image5')
   imageHolder.setAttribute('src', 'images/039.png');
-}
-
-
-
-//set interval for when image changes
-//change image
-//assign image to each button
-function languagesTab(event) {
-  var result = event.target.matches('.tab');
-  var i;
-  var j;
-  if (result) {
-    for (i = 0; i < tabNodeList.length; i++) {
-      if (tabNodeList[i] === event.target) {
-        tabNodeList[i].className = 'tab active';
-        var data = event.target.getAttribute('data-view');
-        for (j = 0; j < viewNodeList.length; j++) {
-          if (viewNodeList[j].getAttribute('data-view') === data) {
-            viewNodeList[j].className = 'view';
-          } else {
-            viewNodeList[j].className = 'hidden';
-          }
-        }
-      } else {
-        tabNodeList[i].className = 'tab';
-      }
-    }
-  }
-
+  clearInterval(pictureChange);
+  i = 4;
+  setInterval(imageLoopAround, 3000)
 }
