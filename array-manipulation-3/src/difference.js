eb7834f261dcdd1218c24cf5e115f6161de547c4
Frank Bishop <ffbishop3@gmail.com>
2021-02-19 12:24:01 -0800

    in progress Array 3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,16 @@
 /* exported difference */
+function difference(first, second) {
+  var newArray = [];
+  var i;
+  for (i = 0; i <= first.length; i++) {
+    if (!second.includes(first[i])) {
+      // newArray.push(first[i]);
+      // console.log(newArray)
+    // } else if(!first.includes(second[i])) {
+    } else {
+      newArray.push(second[i]);
+      // console.log(newArray)
+    }
+  }
+  return newArray;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,11 @@
 /* exported union */
+function union(first, second) {
+  var newArray = [];
+  var i;
+  for (i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      newArray.push(first[i]);
+    } else if (!second.includes(first[i]) || first.includes(second[i])) { newArray.push(second[i]); }
+  }
+  return newArray;
+}
