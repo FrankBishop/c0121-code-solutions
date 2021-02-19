053b8cdbf5cbf8bf29e7a401df9706c7867f11c9
Frank Bishop <ffbishop3@gmail.com>
2021-02-19 10:15:45 -0800

    finished object-manipulation-2

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,26 +1,9 @@
 /* exported defaults */
-// function defaults(target, source) {
-//   var targetProperties = Object.keys(target);
-//   var targetValues = Object.values(target);
-//   var sourceProperties = Object.keys(target);
-//   var sourceValues = Object.values(source);
-//   var i;
-//   for (i = 0; i < sourceProperties.length; i++) {
-//     console.log(sourceProperties)
-//     if (targetProperties.includes(sourceProperties[i])) {
-//       sourceProperties.splice(i, 1);
-//       console.log(sourceProperties)
-//     }
-//   }
-
-// }
-
-// function defaults(target, source) {
-//   var newObject = target
-//   var key;
-//   for (key in source) {
-//     if (!newObject.hasOwnProperty(key)) {
-//       newObject[key] = source[key]
-//     }
-//   }
-// }
+function defaults(target, source) {
+  var key;
+  for (key in source) {
+    if (!target.hasOwnProperty(key)) {
+      target[key] = source[key]
+    }
+  }
+}
