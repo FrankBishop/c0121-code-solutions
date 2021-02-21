4311b7c2f0dc30b180b038620331d183493935ca
FrankBishop <ffbishop3@gmail.com>
2021-02-20 18:13:28 -0800

    finished string manipulation-3

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -3,14 +3,14 @@ function isPalindromic(string) {
   var forward = string;
   var reverse = '';
   var i;
-  for(i=string.length-1; i >= 0; i--){
-    reverse+=string[i];
+  for (i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
   }
   forward = forward.split(" ");
   forward = forward.join('');
   reverse = reverse.split(" ");
   reverse = reverse.join('');
-  if(forward === reverse){
+  if (forward === reverse) {
     return true
   } else {
     return false
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -8,26 +8,43 @@ function titleCase(title) {
   for (i = 0; i < length; i++) {
     newWord = splitTitle[i];
     newWord = newWord.toLowerCase();
-    // newWord = newWord.toLowerCase();
-    console.log(newWord)
-
     if (newWord === 'javascript') {
       newWord = 'JavaScript';
       newTitle = newTitle + ' ' + newWord;
-    } else if(newWord === "javascript:") {
+    } else if (newWord === "javascript:") {
       newWord = 'JavaScript:';
       newTitle = newTitle + ' ' + newWord;
+    } else if (newWord === "api") {
+      newWord = 'API';
+      newTitle = newTitle + ' ' + newWord;
+    } else if (newWord === "web") {
+      newWord = 'Web';
+      newTitle = newTitle + ' ' + newWord;
+    } else if (newWord === "in-depth") {
+      newWord = "In-Depth";
+      newTitle = newTitle + ' ' + newWord;
     }
-    else if (newWord.length > 3){
-      // newWord = newWord.toLowerCase();
+    else if (newWord.length > 3) {
       var upperCase = newWord.slice(0, 1);
       var lowercase = newWord.slice(1);
       upperCase = upperCase.toUpperCase();
       newWord = upperCase + lowercase;
       newTitle = newTitle + ' ' + newWord;
     } else if (newWord.length <= 3) {
-      // newWord = newWord.toLowerCase();
       newTitle = newTitle + ' ' + newWord;
+    }
+  }
+  if (newTitle.includes(':')) {
+    for (i = 0; i < newTitle.length; i++) {
+      if (newTitle[i] === ':') {
+        var subtitle = newTitle.slice(0, i + 2);
+        var subtitle2 = newTitle.slice(i + 2);
+        var subtitle2cap = subtitle2.slice(0, 1);
+        var subtitle2rest = subtitle2.slice(1);
+        subtitle2cap = subtitle2cap.toUpperCase();
+        newTitle = subtitle + subtitle2cap + subtitle2rest;
+
+      }
     }
   }
   newTitle = newTitle.trim();
@@ -35,25 +52,4 @@ function titleCase(title) {
 }
 
 
-// find way to detect what character comes before a word, if empty space or not
-//substring method
 
-
-// function capitalizeWords(string) {
-//   var splitString = string.split(' ');
-//   var newString = '';
-//   var newWord = '';
-//   var i;
-//   var length = splitString.length;
-//   for (i = 0; i < length; i++) {
-//     newWord = splitString[i];
-//     newWord = newWord.toLowerCase();
-//     var upperCase = newWord.slice(0, 1);
-//     var lowercase = newWord.slice(1);
-//     upperCase = upperCase.toUpperCase();
-//     newWord = upperCase + lowercase;
-//     newString = newString + ' ' + newWord;
-//   }
-//   newString = newString.trim();
-//   return newString;
-// }
