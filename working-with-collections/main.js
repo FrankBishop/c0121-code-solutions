f5cc10edf13111173638510f17570e373eff9de8
FrankBishop <ffbishop3@gmail.com>
2021-03-04 17:42:42 -0800

    created deck with loops

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -19,219 +19,25 @@ var players = [
   }
 ]
 
-var cards = [
-  {
-    rank: 'ace',
-    suit: 'clubs'
-  },
-  {
-    rank: 2,
-    suit: 'clubs'
-  },
-  {
-    rank: 3,
-    suit: 'clubs'
-  },
-  {
-    rank: 4,
-    suit: 'clubs'
-  },
-  {
-    rank: 5,
-    suit: 'clubs'
-  },
-  {
-    rank: 6,
-    suit: 'clubs'
-  },
-  {
-    rank: 7,
-    suit: 'clubs'
-  },
-  {
-    rank: 8,
-    suit: 'clubs'
-  },
-  {
-    rank: 9,
-    suit: 'clubs'
-  },
-  {
-    rank: 10,
-    suit: 'clubs'
-  },
-  {
-    rank: 'jack',
-    suit: 'clubs'
-  },
-  {
-    rank: 'queen',
-    suit: 'clubs'
-  },
-  {
-    rank: 'king',
-    suit: 'clubs'
-  },
-  {
-    rank: 'ace',
-    suit: 'diamonds'
-  },
-  {
-    rank: 2,
-    suit: 'diamonds'
-  },
-  {
-    rank: 3,
-    suit: 'diamonds'
-  },
-  {
-    rank: 4,
-    suit: 'diamonds'
-  },
-  {
-    rank: 5,
-    suit: 'diamonds'
-  },
-  {
-    rank: 6,
-    suit: 'diamonds'
-  },
-  {
-    rank: 7,
-    suit: 'diamonds'
-  },
-  {
-    rank: 8,
-    suit: 'diamonds'
-  },
-  {
-    rank: 9,
-    suit: 'diamonds'
-  },
-  {
-    rank: 10,
-    suit: 'diamonds'
-  },
-  {
-    rank: 'jack',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'queen',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'king',
-    suit: 'diamonds'
-  },
-  {
-    rank: 'ace',
-    suit: 'hearts'
-  },
-  {
-    rank: 2,
-    suit: 'hearts'
-  },
-  {
-    rank: 3,
-    suit: 'hearts'
-  },
-  {
-    rank: 4,
-    suit: 'hearts'
-  },
-  {
-    rank: 5,
-    suit: 'hearts'
-  },
-  {
-    rank: 6,
-    suit: 'hearts'
-  },
-  {
-    rank: 7,
-    suit: 'hearts'
-  },
-  {
-    rank: 8,
-    suit: 'hearts'
-  },
-  {
-    rank: 9,
-    suit: 'hearts'
-  },
-  {
-    rank: 10,
-    suit: 'hearts'
-  },
-  {
-    rank: 'jack',
-    suit: 'hearts'
-  },
-  {
-    rank: 'queen',
-    suit: 'hearts'
-  },
-  {
-    rank: 'king',
-    suit: 'hearts'
-  },
-  {
-    rank: 'ace',
-    suit: 'spades'
-  },
-  {
-    rank: 2,
-    suit: 'spades'
-  },
-  {
-    rank: 3,
-    suit: 'spades'
-  },
-  {
-    rank: 4,
-    suit: 'spades'
-  },
-  {
-    rank: 5,
-    suit: 'spades'
-  },
-  {
-    rank: 6,
-    suit: 'spades'
-  },
-  {
-    rank: 7,
-    suit: 'spades'
-  },
-  {
-    rank: 8,
-    suit: 'spades'
-  },
-  {
-    rank: 9,
-    suit: 'spades'
-  },
-  {
-    rank: 10,
-    suit: 'spades'
-  },
-  {
-    rank: 'jack',
-    suit: 'spades'
-  },
-  {
-    rank: 'queen',
-    suit: 'spades'
-  },
-  {
-    rank: 'king',
-    suit: 'spades'
+var deck = [];
+var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+var ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king', 'ace'];
+
+function createDeck() {
+  for (var j = 0; j < suits.length; j++) {
+    for (var i = 0; i < ranks.length; i++) {
+      var card = {};
+      card.suit = suits[j]
+      card.rank = ranks[i]
+      deck.unshift(card)
+    }
   }
-]
+  return deck
+}
 
+createDeck();
 
-var shuffle = _.shuffle(cards);
+var shuffle = _.shuffle(deck);
 
 var player1Hand = _.dropRight(shuffle, 50)
 var player2PreHand = _.drop(shuffle, 2)
@@ -261,8 +67,8 @@ function findScore(hand) {
   return playerScore
 }
 
-var player1Score = findScore (players[0].hand)
-var player2Score = findScore (players[1].hand)
+var player1Score = findScore(players[0].hand)
+var player2Score = findScore(players[1].hand)
 var player3Score = findScore(players[2].hand)
 var player4Score = findScore(players[3].hand)
 
