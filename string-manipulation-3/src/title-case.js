36757fd24872fd7388538518251dbaecd2790dc5
FrankBishop <ffbishop3@gmail.com>
2021-03-04 19:03:37 -0800

    fixed Title Case

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -4,11 +4,12 @@ function titleCase(title) {
   var newTitle = '';
   var newWord = '';
   var length = splitTitle.length;
+  var previousWord = '';
   for (var i = 0; i < length; i++) {
     newWord = splitTitle[i];
     newWord = newWord.toLowerCase();
     if (newWord === 'javascript') {
-      newWord = 'JavaScript';
+      newWord = 'JavaScript'
       newTitle = newTitle + ' ' + newWord;
     } else if (newWord === "javascript:") {
       newWord = 'JavaScript:';
@@ -16,6 +17,28 @@ function titleCase(title) {
     } else if (newWord === "api") {
       newWord = 'API';
       newTitle = newTitle + ' ' + newWord;
+    } else if (newWord.includes('-')) {
+      for (var j = 0; j < newWord.length; j++) {
+        if (newWord[j] === '-') {
+          var dashWord = newWord.slice(0, j + 1);
+          var dashWord2 = newWord.slice(j + 1);
+          var dashWord1cap = dashWord.slice(0, 1);
+          var dashWord1rest = dashWord.slice(1)
+          var dashWord2cap = dashWord2.slice(0, 1);
+          var dashWord2rest = dashWord2.slice(1);
+          dashWord1cap = dashWord1cap.toUpperCase();
+          dashWord2cap = dashWord2cap.toUpperCase();
+          newWord = dashWord1cap + dashWord1rest + dashWord2cap + dashWord2rest
+          newTitle = newTitle + ' ' + newWord;
+        }
+      }
+    } else if (previousWord.includes(':')) {
+      var upperCase = newWord.slice(0, 1);
+      var lowercase = newWord.slice(1);
+      upperCase = upperCase.toUpperCase();
+      newWord = upperCase + lowercase;
+      newTitle = newTitle + ' ' + newWord;
+
     } else if (newWord === 'and' || newWord === 'or' || newWord === 'nor' || newWord === 'but' || newWord === 'a' || newWord === 'the' || newWord === 'an' || newWord === 'as' || newWord === 'by' || newWord === 'for' || newWord === 'in' || newWord === 'of' || newWord === 'on' || newWord === 'per' || newWord === 'to') {
       newTitle = newTitle + ' ' + newWord;
     }
@@ -26,34 +49,7 @@ function titleCase(title) {
       newWord = upperCase + lowercase;
       newTitle = newTitle + ' ' + newWord;
     }
-  }
-  if (newTitle.includes(':')) {
-    for (i = 0; i < newTitle.length; i++) {
-      if (newTitle[i] === ':') {
-        var subtitle = newTitle.slice(0, i + 2);
-        var subtitle2 = newTitle.slice(i + 2);
-        var subtitle2cap = subtitle2.slice(0, 1);
-        var subtitle2rest = subtitle2.slice(1);
-        subtitle2cap = subtitle2cap.toUpperCase();
-        newTitle = subtitle + subtitle2cap + subtitle2rest;
-
-      }
-    }
-  }
-  if (newTitle.includes('-')) {
-    for (i = 0; i < newTitle.length; i++) {
-      if (newTitle[i] === '-') {
-        var dashTitle = newTitle.slice(0, i + 1);
-        var dashTitle2 = newTitle.slice(i + 1);
-        var dashTitle1cap = dashTitle.slice(0, 1);
-        var dashTitle1rest = dashTitle.slice(1)
-        var dashTitle2cap = dashTitle2.slice(0, 1);
-        var dashTitle2rest = dashTitle2.slice(1);
-        dashTitle1cap = dashTitle1cap.toUpperCase();
-        dashTitle2cap = dashTitle2cap.toUpperCase();
-        newTitle = dashTitle1cap + dashTitle1rest + dashTitle2cap + dashTitle2rest;
-      }
-    }
+    previousWord = newWord
   }
   newTitle = newTitle.trim();
   return newTitle
