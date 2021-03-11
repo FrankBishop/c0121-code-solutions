87852d788b7f7a5e7e539d0343595a424fd880c1
FrankBishop <ffbishop3@gmail.com>
2021-03-11 14:44:48 -0800

    feature 2 finished, User can add a note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,7 +1,7 @@
 // const { listenerCount } = require('events');
 const fs = require('fs');
 const dataJSON = require('./data.json');
-
+// const todo = require('./todo.txt');
 // fs.writeFile('todo.txt', list, 'utf8', err => {
 //   if (err) throw err;
 // });
@@ -16,4 +16,25 @@ if (process.argv[2] === 'read') {
       console.log(`${key}: ${dataJSON.notes[key]}`);
     }
   });
+} else if (process.argv[2] === 'create') {
+  // console.log(dataJSON);
+  // let todos = JSON.stringify(dataJSON.notes, null, 2)
+  // console.log(todos);
+  const todoToAdd = process.argv[3];
+  addTodo(todoToAdd);
+  console.log(dataJSON);
+  const todos = JSON.stringify(dataJSON, null, 2);
+  // let updatedTodos = JSON.parse(todos);
+  fs.writeFile('data.json', todos, 'utf8', err => {
+    if (err) {
+      throw err;
+    }
+  });
+}
+
+// let todos = JSON.stringify(data.JSON, null, 2)
+// maybe set result of for in loop to a variable to call later in other functions
+function addTodo(addedTodo) {
+  dataJSON.notes[dataJSON.nextId] = addedTodo;
+  dataJSON.nextId++;
 }
