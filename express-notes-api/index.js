7a800308bc48f68c4826c5806799d0d4463225f3
FrankBishop <ffbishop3@gmail.com>
2021-03-17 15:52:56 -0700

    fix in progress

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -17,34 +17,33 @@ app.get('/api/notes', function (req, res) {
   res.status(200).send(array);
 });
 
-app.get('/api/notes/:id', function (req, res) {
-  console.log('it runs');
-  const id = req.params.id;
-  if (id < 0) {
-    res.status(400).send({ "error": "id must be a positive integer" })
-  } else if (!dataJSON.notes.hasOwnProperty(id)) {
-    res.status(404).send({ "error": "cannot find note with id" + ' ' + id });
-  } else {
-    res.status(200).send(dataJSON.notes[id]);
-  }
-});
+// app.get('/api/notes/:id', function (req, res) {
+//   const id = req.params.id;
+//   if (id < 0) {
+//     res.status(400).send({ "error": "id must be a positive integer" })
+//   } else if (!dataJSON.notes.hasOwnProperty(id)) {
+//     res.status(404).send({ "error": "cannot find note with id" + ' ' + id });
+//   } else {
+//     res.status(200).send(dataJSON.notes[id]);
+//   }
+// });
 
-app.post('/api/notes', function (req, res, next) {
-  if (req.body.hasOwnProperty('content')) {
-    req.body.id = nextId;
-    dataJSON.notes[nextId] = req.body;
-    res.status(201).json(req.body);
-    nextId++;
-    const todos = JSON.stringify(dataJSON, null, 2);
-    fs.writeFile('data.json', todos, 'utf8', err => {
-      if (err) {
-        res.status(500).send({ "error": "An unexpected error occurred." });
-      }
-    });
-  } else {
-    res.status(400).send({ "error": "content is a required field" })
-  }
-});
+// app.post('/api/notes', function (req, res, next) {
+//   if (req.body.hasOwnProperty('content')) {
+//     req.body.id = nextId;
+//     dataJSON.notes[nextId] = req.body;
+//     res.status(201).json(req.body);
+//     nextId++;
+//     const todos = JSON.stringify(dataJSON, null, 2);
+//     fs.writeFile('data.json', todos, 'utf8', err => {
+//       if (err) {
+//         res.status(500).send({ "error": "An unexpected error occurred." });
+//       }
+//     });
+//   } else {
+//     res.status(400).send({ "error": "content is a required field" })
+//   }
+// });
 
 app.delete('/api/notes/:id', function (req, res) {
   const id = req.params.id;
@@ -68,30 +67,30 @@ app.delete('/api/notes/:id', function (req, res) {
   };
 });
 
-app.put('/api/notes/:id', function (req, res) {
-  const id = req.params.id;
-  if (id < 0) {
-    res.status(400).send({ "error": "id must be a positive integer" });
-  } else if ((req.body.hasOwnProperty('content'))) {
-    let key;
-    for (key in dataJSON.notes) {
-      if (id === key) {
-        req.body.id = id;
-        dataJSON.notes[key] = req.body;
-        res.status(200).send(dataJSON.notes[key]);
-        const todos = JSON.stringify(dataJSON, null, 2);
-        fs.writeFile('data.json', todos, 'utf8', err => {
-          if (err) {
-            res.status(500).send({ "error": "An unexpected error occurred." });
-          }
-        });
-      }
-    }
-    res.status(404).send({ "error": "cannot find note with id" + ' ' + id });
-  } else {
-    res.status(400).send({ "error": "content is a required field" });
-  }
-});
+// app.put('/api/notes/:id', function (req, res) {
+//   const id = req.params.id;
+//   if (id < 0) {
+//     res.status(400).send({ "error": "id must be a positive integer" });
+//   } else if ((req.body.hasOwnProperty('content'))) {
+//     // let key;
+//     for (key in dataJSON.notes) {
+//       if (id === key) {
+//         req.body.id = id;
+//         dataJSON.notes[key] = req.body;
+//         res.status(200).send(dataJSON.notes[key]);
+//         const todos = JSON.stringify(dataJSON, null, 2);
+//         fs.writeFile('data.json', todos, 'utf8', err => {
+//           if (err) {
+//             res.status(500).send({ "error": "An unexpected error occurred." });
+//           }
+//         });
+//       }
+//     }
+//     res.status(404).send({ "error": "cannot find note with id" + ' ' + id });
+//   } else {
+//     res.status(400).send({ "error": "content is a required field" });
+//   }
+// });
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
