96e8b9ac71001ae123abd7610789422e1e672b6f
FrankBishop <ffbishop3@gmail.com>
2021-03-19 14:54:38 -0700

    fully functioning first feature

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -17,7 +17,6 @@ app.listen(3000, () => {
 });
 
 app.get('/api/grades', function (req, res) {
-  console.log('this runs')
   const sql = `
     select *
       from "grades"
@@ -25,34 +24,7 @@ app.get('/api/grades', function (req, res) {
   db.query(sql)
     .then(result => {
       const grade = result.rows;
-      res.json(grade);
-    });
-});
-
-app.get('/api/grades/:gradeId', (req, res, next) => {
-  const gradeId = parseInt(req.params.gradeId, 10);
-  if (!Number.isInteger(gradeId) || gradeId <= 0) {
-    res.status(400).json({
-      error: '"gradeId" must be a positive integer'
-    });
-    return;
-  }
-  const sql = `
-    select *
-      from "grades"
-     where "gradeId" = $1
-  `;
-  const params = [gradeId];
-  db.query(sql, params)
-    .then(result => {
-      const grade = result.rows[0];
-      if (!grade) {
-        res.status(404).json({
-          error: `Cannot find grade with "gradeId" ${gradeId}`
-        });
-      } else {
-        res.json(grade);
-      }
+      res.status(200).json(grade);
     })
     .catch(err => {
       console.error(err);
@@ -61,3 +33,36 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
       });
     });
 });
+
+// app.get('/api/grades/:gradeId', (req, res, next) => {
+//   const gradeId = parseInt(req.params.gradeId, 10);
+//   if (!Number.isInteger(gradeId) || gradeId <= 0) {
+//     res.status(400).json({
+//       error: '"gradeId" must be a positive integer'
+//     });
+//     return;
+//   }
+//   const sql = `
+//     select *
+//       from "grades"
+//      where "gradeId" = $1
+//   `;
+//   const params = [gradeId];
+//   db.query(sql, params)
+//     .then(result => {
+//       const grade = result.rows[0];
+//       if (!grade) {
+//         res.status(404).json({
+//           error: `Cannot find grade with "gradeId" ${gradeId}`
+//         });
+//       } else {
+//         res.json(grade);
+//       }
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({
+//         error: 'An unexpected error occurred.'
+//       });
+//     });
+// });
