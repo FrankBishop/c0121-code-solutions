96e70e37e23cfd1f7e2489955de8e699fa6da437
FrankBishop <ffbishop3@gmail.com>
2021-03-19 17:29:32 -0700

    partially working feature 3

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -37,7 +37,7 @@ app.get('/api/grades', function (req, res) {
 app.post('/api/grades', function (req, res) {
   if (req.body.score < 0) {
     res.status(400).json({
-      error: '"gradeId" must be a positive integer'
+      error: '"score" must be a positive integer'
     });
     return;
   } else if (!req.body.name) {
@@ -48,6 +48,11 @@ app.post('/api/grades', function (req, res) {
   } else if (!req.body.course) {
     res.status(400).json({
       error: '"course" is a required field'
+    });
+    return;
+  } else if (!req.body.score) {
+    res.status(400).json({
+      error: '"score" is a required field'
     });
     return;
   }
@@ -73,6 +78,96 @@ app.post('/api/grades', function (req, res) {
       });
     });
 });
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const id = req.params.gradeId;
+  let test = false;
+  if (id < 0) {
+    res.status(400).json({
+      error: '"gradeId" must be a positive integer'
+    });
+    return;
+  } else if (!req.body.name) {
+    res.status(400).json({
+      error: '"name" is a required field'
+    });
+    return;
+  } else if (!req.body.course) {
+    res.status(400).json({
+      error: '"course" is a required field'
+    });
+    return;
+  } else if (!req.body.score) {
+    res.status(400).json({
+      error: '"score" is a required field'
+    });
+    return;
+  }
+  const values = [];
+  let key;
+  for (const key in req.body) {
+    values.push(`${req.body[key]}`);
+  }
+  console.log(values);
+  const sql = `
+    update "grades"
+       set "course" = $1,
+           "name" = $2,
+           "score" = $3
+     where "gradeId" = ${id}
+     returning *;
+  `;
+  db.query(sql, values)
+    .then(result => {
+      const grade = result.rows[0];
+      res.status(200).json(grade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//   else if (req.body.content) {
+//     let key;
+//     for (key in dataJSON.notes) {
+//       if (id === key) {
+//         req.body.id = id;
+//         dataJSON.notes[key] = req.body;
+//         const todos = JSON.stringify(dataJSON, null, 2);
+//         test = true;
+//         fs.writeFile('data.json', todos, 'utf8', err => {
+//           if (err) {
+//             res.status(500).send({ error: 'An unexpected error occurred.' });
+//           } else {
+//             res.status(200).send(dataJSON.notes[key]);
+//           }
+//         });
+//       }
+//     }
+//     if (test === false) {
+//       res.status(404).send({ error: 'cannot find note with id' + ' ' + id });
+//     }
+//   } else {
+//     res.status(400).send({ error: 'content is a required field' });
+//   }
+// });
+
+
+
 
 
 // app.get('/api/grades/:gradeId', (req, res, next) => {
