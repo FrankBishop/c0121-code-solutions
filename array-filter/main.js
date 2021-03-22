50cb625dd2b61a9278f62a21ce3ed54a6a6504e8
FrankBishop <ffbishop3@gmail.com>
2021-03-22 10:49:29 -0700

    fixed array-filter

--- array-filter/main.js
+++ array-filter/main.js
@@ -1,7 +1,3 @@
-// const { connectableObservableDescriptor } = require("rxjs/internal/observable/ConnectableObservable");
-
-// const { over } = require("lodash");
-
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 const names = [
