b2f4dafd3d0a34badd446de64266ee022b618d4b
FrankBishop <ffbishop3@gmail.com>
2021-03-22 17:22:20 -0700

    fixed array-reduce

--- array-reduce/main.js
+++ array-reduce/main.js
@@ -22,7 +22,6 @@ console.log('The sum of the numbers is', numbers.reduce(sum));
 const product = (accumulator, number) => accumulator * number;
 console.log('The product of all numbers is', numbers.reduce(product));
 const balance = function (accumulator, transaction, initialValue) {
-  accumulator += 0;
   if (transaction['type'] === 'deposit') {
     return accumulator += transaction['amount']
   } else if (transaction['type'] === 'withdrawal') {
