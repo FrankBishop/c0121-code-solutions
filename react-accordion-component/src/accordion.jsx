505ff1f5bbf3e0a43e00e05ddd9c71ac497ca177
FrankBishop <ffbishop3@gmail.com>
2021-03-26 18:03:23 -0700

    in progress accodion

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,30 +1,49 @@
 import React from 'react';
 
+
 class Accordion extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { menuOpen: false };
+    this.state = { isClicked: false };
     this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
+    if (this.state.isClicked === true) {
+      const header = this.state.header
+      const languages = this.props.languages;
+      const ListItems = languages.map((language) => {
+        console.log(language.language);
+        if (header === language.language) {
+          console.log(header);
+          <div key={language.language} >
+            <h2 onClick={this.handleClick}> {language.language} </h2>
+            <li>
+              {language.info}</li>
+          </div >;
+        };
+      });
+      console.log(ListItems)
+      return (
+        <ul>
+          {ListItems}
+        </ul>
+      );
+    }
     return <div>
       <h2 onClick={this.handleClick}>Hypertext Markup Language</h2>
       <h2 onClick={this.handleClick}>Cascading Style Sheets</h2>
       <h2 onClick={this.handleClick}>JavaScript</h2>
-    </div>;
+    </div >;
+
   }
 
   handleClick(event) {
-    for (let i = 0; i < 3; i++) {
-      console.log(this.props[i]);
-    }
+    this.setState({ isClicked: true, header: event.target.textContent });
+
   }
 
 }
 
 export default Accordion;
-
-
-// handle click changes state, once state is changed, render loops through to determine what to show
