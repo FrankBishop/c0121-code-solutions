952e01a1c87c7f2d44f7cdbcfeb3a02c1ae1cf12
FrankBishop <ffbishop3@gmail.com>
2021-03-29 11:09:41 -0700

    fixed express static

--- express-static/index.js
+++ express-static/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const joinedPath = path.join(__dirname , 'public');
-const staticTest = app.use(express.static('public'));
+const staticTest = app.use(express.static(joinedPath));
 
 
 app.listen(3000, () => {
