c4a6f3d9f80d0ddcda17017f778c65dbb646f8c9
FrankBishop <ffbishop3@gmail.com>
2021-04-26 19:03:48 -0700

    finished accordion

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,47 +1,37 @@
 import React from 'react';
 
-
 class Accordion extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = { languageClicked: null };
     this.handleClick = this.handleClick.bind(this);
+    this.close = this.close.bind(this);
   }
 
   render() {
-    if (this.state.isClicked === true) {
-      const header = this.state.header
-      const languages = this.props.languages;
-      const ListItems = languages.map((language) => {
-        console.log(language.language);
-        if (header === language.language) {
-          console.log(header);
-          <div key={language.language} >
-            <h2 onClick={this.handleClick}> {language.language} </h2>
-            <li>
-              {language.info}</li>
-          </div >;
-        };
-      });
-      console.log(ListItems)
-      return (
-        <ul>
-          {ListItems}
-        </ul>
-      );
-    }
+    const accordList = this.props.languages.map(language =>
+      <div number={language.number} key={language.number}>
+        <h2 onClick={this.handleClick} onDoubleClick={this.close}>{language.language}</h2>
+        {this.state.languageClicked === language.number &&
+          <p>{language.info}</p>
+        }
+      </div>
+    );
     return <div>
-      <h2 onClick={this.handleClick}>Hypertext Markup Language</h2>
-      <h2 onClick={this.handleClick}>Cascading Style Sheets</h2>
-      <h2 onClick={this.handleClick}>JavaScript</h2>
+      {accordList}
     </div >;
 
   }
 
   handleClick(event) {
-    this.setState({ isClicked: true, header: event.target.textContent });
+    let accordNumber = event.target.parentElement.getAttribute('number');
+    accordNumber = Number(accordNumber);
+    this.setState({ languageClicked: accordNumber });
+  }
 
+  close() {
+    this.setState({ languageClicked: null });
   }
 
 }
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -4,14 +4,17 @@ import Accordion from './accordion.jsx';
 
 const languages = [
   {
+    number: 1,
     language: 'Hypertext Markup Language',
     info: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications.  With Cascading Style Sheets(CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.'
   },
   {
+    number: 2,
     language: 'Cascading Style Sheets',
     info: 'Cascading Style Sheets (CSS) is a style sheet language used for descrbing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript. '
   },
   {
+    number: 3,
     language: 'JavaScript',
     info: 'JavaScript, often abbreviated as JS, is a high level, interpreted programming language that conforms to the ECMAScript specification.  JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
   }
